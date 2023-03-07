170edf7b26b0b424a172f1ce3bedf848bb83eea9
Geoff Pleiss <gpleiss@gmail.com>
2023-03-06 20:45:26 -0800

    Updates

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import FontAwesomeIcon from '@fortawesome/react-fontawesome';
-import { faBook, faFileCode, faChartLine, faPuzzlePiece, faStopwatch } from '@fortawesome/fontawesome-free-solid';
+import { faBook, faFileCode } from '@fortawesome/fontawesome-free-solid';
+//import { faChartLine, faPuzzlePiece, faStopwatch } from '@fortawesome/fontawesome-free-solid';
 import logo from '../images/pytorch.png';
 import kilian from '../images/kilian.png';
 import andrew from '../images/andrew.jpg';
 import max from '../images/max.jpg';
 import background from '../images/background.png';
-import facebook_logo from '../images/facebook_logo.png'
-import uber_ai_horizontal from '../images/uber_ai_horizontal.png'
+//import facebook_logo from '../images/facebook_logo.png'
+//import uber_ai_horizontal from '../images/uber_ai_horizontal.png'
 import hljs from 'highlight.js';
 import ReactDOM from 'react-dom';
 
@@ -78,14 +79,11 @@ class Home extends React.Component {
 
     return (
       <div>
-        <div className="jumbotron jumbotron-fluid shadow-bottom mb-8" style={backgroundStyle}>
+        <div className="jumbotron jumbotron-fluid shadow-bottom" style={backgroundStyle}>
           <div className="container text-center">
             <div className="row">
               <div className="col-12">
-                <h1 className="display-3 font-weight-bold">GPyTorch</h1>
-                <h2 className="font-weight-light h3">
-                  Gaussian processes for <em>modern</em> machine learning systems.
-                </h2>
+                <h1 className="display-3 font-weight-bold" style={{paddingBottom: "90px"}}>GPyTorch</h1>
                 <div className="m-4">
                   <img style={{maxHeight: "150px"}} src={logo} alt="logo" />
                 </div>
@@ -97,69 +95,6 @@ class Home extends React.Component {
             </div>
           </div>
         </div>
-
-        <div className="container mt-8 mb-8">
-          <div className="row">
-            <div className="col-12 col-md-6 col-lg-4">
-              <div className="text-center">
-                <FontAwesomeIcon icon={faChartLine} size="3x" title="About" className="align-middle" />
-              </div>
-              <div className="text-center ml-3 mr-3">
-                <h3>Scalablity</h3>
-                <p>
-                  Train Gaussian processes with <em>millions</em> of data points.
-                </p>
-              </div>
-            </div>
-
-            <div className="col-12 col-md-6 col-lg-4">
-              <div className="text-center">
-                <FontAwesomeIcon icon={faPuzzlePiece} size="3x" title="About" className="align-middle" />
-              </div>
-              <div className="text-center ml-3 mr-3">
-                <h3>Modular Design</h3>
-                <p>
-                  Combine Gaussian processes with <em>deep neural networks</em> and more.
-                </p>
-              </div>
-            </div>
-
-            <div className="col-12 col-md-6 offset-md-3 offset-lg-0 col-lg-4">
-              <div className="text-center">
-                <FontAwesomeIcon icon={faStopwatch} size="3x" title="About" className="align-middle" />
-              </div>
-              <div className="text-center ml-3 mr-3">
-                <h3>Speed</h3>
-                <p>
-                  Utilize <strong>GPU acceleration</strong> and state-of-the-art inference algorithms.
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div id="install" className="jumbotron jumbotron-fluid shadow-top mt-8 mb-0">
-          <div className="container">
-            <div className="row">
-              <div className="col-12 text-center">
-                <h2 className="display-4 font-weight-bold">Installation</h2>
-                <div className="alert alert-warning pt-3 pb-3" role="alert">
-                  <strong>GPyTorch requires Python >= 3.6 and PyTorch >= 1.6</strong>
-                </div>
-                <p className="pt-3">
-                  Make sure you have <a href="https://pytorch.org/get-started/locally/">PyTorch installed</a> (at least version 1.6). Then,
-                </p>
-                <Code className="shell pl-4 pr-4 pt-3 pb-3 mb-3 mt-3">
-pip install gpytorch
-                </Code>
-                <p>
-                  For more instructions, see the <a href="http://github.com/cornellius-gp/gpytorch" rel="noopener noreferrer" target="_blank">Github README</a>.
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-
         <div className="container mt-8 mb-8">
           <div className="row">
             <div className="col-12 col-sm-6 col-md-4 offset-md-2 text-center">
@@ -201,48 +136,80 @@ pip install gpytorch
             </div>
           </div>
         </div>
-
         <div id="install" className="jumbotron jumbotron-fluid shadow-top mt-8 mb-0">
           <div className="container">
             <div className="row">
               <div className="col-12 text-center">
-                <h2 className="display-4 font-weight-bold">The Team</h2>
-              </div>
-            </div>
-            <div className="row mt-3">
-              <div className="col-12 col-md-8 offset-md-2">
-                <ul className="list-group">
-                  <TeamMember name="Geoff Pleiss" href="http://geoffpleiss.com" img="https://avatars2.githubusercontent.com/u/824157?s=460&v=4" />
-                  <TeamMember name="Jacob R. Gardner" href="https://jacobrgardner.github.io/" img="https://jacobrgardner.github.io/images/me.jpg" />
-                  <TeamMember name="Kilian Q. Weinberger" href="http://kilian.cs.cornell.edu/" img={kilian}/>
-                  <TeamMember name="Andrew Gordon Wilson" href="https://people.orie.cornell.edu/andrew/" img={andrew} />
-                  <TeamMember name="Max Balandat" href="https://research.fb.com/people/balandat-max/" img={max} />
-                </ul>
-              </div>
-            </div>
-            <div className="row mt-3">
-              <div className="col-12 col-md-8 offset-md-2">
-                <p className="text-center">
-                  <strong>With (many) contributions from:</strong>
-                  &nbsp;
-                  David Arbour,
-                  Eytan Bakshy,
-                  Karthik Rajkumar,
-                  Sam Stanton,
-                  Ruihan Wu,
-                  Bram Wallace,
-                  Ke Alexander Wang,
-                  Jared Frank,
-                  and others!
+                <h2 className="display-4 font-weight-bold">Installation</h2>
+                <div className="alert alert-warning pt-3 pb-3" role="alert">
+                  <strong>GPyTorch requires Python >= 3.8</strong>
+                </div>
+                <p className="pt-3">
+                  Make sure you have <a href="https://pytorch.org/get-started/locally/">PyTorch installed</a>. Then,
+                </p>
+                <Code className="shell pl-4 pr-4 pt-3 pb-3 mb-3 mt-3">
+  pip install gpytorch
+                </Code>
+                <p>
+                  For more instructions, see the <a href="http://github.com/cornellius-gp/gpytorch" rel="noopener noreferrer" target="_blank">Github README</a>.
                 </p>
               </div>
             </div>
-            <div className="row mt-5">
-              <div className="col-12 col-md-8 offset-md-2 text-center">
-                <figure>
-                  <figcaption className="figure-caption">Developed at Cornell University, Columbia University, University of Pennsylvania, New York University, and Facebook, with funding from the <a href="https://www.gatesfoundation.org/" target="_blank" rel="noopener noreferrer">Bill and Melinda Gates Foundation</a>, the <a href="https://www.nsf.gov/" target="_blank" rel="noopener noreferrer">National Science Foundation</a>, and <a href="https://www.sap.com/index.html" target="_blank" rel="noopener noreferrer">SAP</a>.</figcaption>
-                </figure>
-              </div>
+          </div>
+        </div>
+        <div className="container pt-8">
+          <div className="row">
+            <div className="col-12 text-center">
+              <h2 className="display-4 font-weight-bold">The Team</h2>
+            </div>
+          </div>
+          <div className="row mt-3">
+            <div className="col-12 col-md-8 offset-md-2">
+              <ul className="list-group">
+                <TeamMember name="Geoff Pleiss" href="http://geoffpleiss.com" img="https://avatars2.githubusercontent.com/u/824157?s=460&v=4" />
+                <TeamMember name="Jacob R. Gardner" href="https://jacobrgardner.github.io/" img="https://jacobrgardner.github.io/images/me.jpg" />
+                <TeamMember name="Kilian Q. Weinberger" href="http://kilian.cs.cornell.edu/" img={kilian}/>
+                <TeamMember name="Andrew Gordon Wilson" href="https://people.orie.cornell.edu/andrew/" img={andrew} />
+                <TeamMember name="Max Balandat" href="https://research.fb.com/people/balandat-max/" img={max} />
+              </ul>
+            </div>
+          </div>
+          <div className="row mt-3">
+            <div className="col-12 col-md-8 offset-md-2">
+              <p className="text-center">
+                <strong>With (many) contributions from:</strong>
+                &nbsp;
+                Eytan Bakshy,
+                Wesley Maddox,
+                Ke Alexander Wang,
+                Ruihan Wu,
+                Sait Cakmak,
+                David Eriksson,
+                Sam Daulton,
+                Martin Jankowiak,
+                Sam Stanton,
+                Zitong Zhou,
+                David Arbour,
+                Karthik Rajkumar,
+                Bram Wallace,
+                Jared Frank,
+                and others!
+              </p>
+            </div>
+          </div>
+          <div className="row mt-5">
+            <div className="col-12 col-md-8 offset-md-2 text-center">
+              <figure>
+                <figcaption className="figure-caption">
+                  Developed at Cornell University, Columbia University, University of Pennsylvania, New York University, and Meta,
+                  with funding from
+                  the <a href="https://www.gatesfoundation.org/" target="_blank" rel="noopener noreferrer">Bill and Melinda Gates Foundation</a>,
+                  the <a href="https://www.nsf.gov/" target="_blank" rel="noopener noreferrer">National Science Foundation</a>,
+                  <a href="https://www.sap.com/index.html" target="_blank" rel="noopener noreferrer">SAP</a>,
+                  the <a href="https://www.simonsfoundation.org/" target="_blank" rel="noopener noreferrer">Simons Foundation</a>, and
+                  the <a href="https://www.gatsby.org.uk/" target="_blank" rel="noopener noreferrer">Gatsby Charatable Trust</a>.
+                </figcaption>
+              </figure>
             </div>
           </div>
         </div>
